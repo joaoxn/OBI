c93f8ccac1508ad7bf551155eb70c436f39b93d7
João Victor Olivo <jvso@yahoo.com>
2026-06-12 00:27:47 -0300

    update ralouim

--- Problemas/Prep OBI/Treino 10/ralouim.cpp
+++ Problemas/Prep OBI/Treino 10/ralouim.cpp
@@ -1,8 +1,10 @@
+//TODO: Resolver (usando DP)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define debug(args...) printf(args)
-#define debugln(args...) debug(args); printf("\n")
+#define debugln(args...) debug(args); debug("\n")
 #define fi first
 #define se second
 
@@ -11,36 +13,32 @@ pair<int,int> p[2001];
 
 int main() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> p[i].first >> p[i].second;
     }
+    p[0] = {0,0};
 
-    int sol = -1;
-    for (int i = 0; i < n; i++) {
-        debugln("start: %d", i);
-        auto curr = i;
-        int last_dist = INT_MAX;
-        int barracas = 1;
-        while (true) {
-            int maxd = -1;
-            int maxj = -1;
-            for (int j = 0; j < n; j++) {
-                if (curr == j) continue;
-
-                int dist = pow(p[i].fi-p[j].fi,2)+pow(p[i].se-p[j].se,2);
-                if (dist > maxd) {
-                    maxd = dist;
-                    maxj = j;
-                }
+    int i = 0;
+    int last_dist = INT_MAX;
+    int barracas = 0;
+    while (true) {
+        int maxd = -1;
+        int maxj = -1;
+        for (int j = 1; j <= n; j++) {
+            if (i == j) continue;
+            int dist = pow(p[i].fi-p[j].fi,2)+pow(p[i].se-p[j].se,2);
+            debugln("%d (%d,%d)-(%d,%d)=%d", j, p[i].fi,p[i].se, p[j].fi,p[j].se, dist);
+            if (dist > maxd && dist < last_dist) {
+                maxd = dist;
+                maxj = j;
             }
-            debugln("longe=%d", maxj);
-            if (maxd == -1 || maxd >= last_dist) break;
-            curr = maxj;
-            last_dist = maxd;
-            barracas++;
         }
-        if (barracas > sol) sol = barracas;
+        debugln("longe=%d", maxj);
+        if (maxd == -1) break;
+        i = maxj;
+        last_dist = maxd;
+        barracas++;
     }
 
-    cout << sol << '\n';
+    cout << barracas << '\n';
 }
